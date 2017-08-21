123f484a74a4fe1c150115a27163a04e1902b236
TylerEbner <30875263+TylerEbner@users.noreply.github.com>
2017-08-21 09:12:35 -1000

    Insult generator
    I added a random insult generator.

---
+++ insultGenerator.js
@@ -0,0 +1,8 @@
+function insult() {
+//I created a function named insult//
+var words = ["You suck", "You stink", "You're ugly"];
+//I created an array with 3 variables//
+window.alert(words[Math.floor(Math.random() * 3)]);
+//I created a window alert and I used Math.floor to round the numbers to the nearest whole number(0-2)//
+//I used Math.random() and I can multiplied by 3 to only get the numbers 0, 1, 2//
+}
